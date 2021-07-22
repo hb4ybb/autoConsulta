49ea3cd306a615b1114095476185e1ce48b447f4
hb4ybb <87793920+hb4ybb@users.noreply.github.com>
2021-07-21 22:48:55 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,9 +1,9 @@
 from paramiko import SSHClient
 import os
 
-IP = ''
-USER = ''
-PASSWORD = ''
+IP = '127.0.0.1'
+USER = 'montana'
+PASSWORD = 'kCk7b5SaBPUkibDzcDa8bcKvb'
 
 client = SSHClient()
 client.load_system_host_keys()
@@ -12,3 +12,13 @@ client.connect(IP, username=USER, password=PASSWORD)
 stdin, stdout, stderr = client.exec_command(
     'cd /montana/anti_hack; chmod 000 anti_hack.txt'
     )
+
+print(f'STDOUT: {stdout.read().decode("utf8")}')
+print(f'STDERR: {stderr.read().decode("utf8")}')
+
+
+stdin.close()
+stdout.close()
+stderr.close()
+
+client.close()
