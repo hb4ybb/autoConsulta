9b042bb98a941f30635b3bbccc5852ee6c1b4fd8
hb4ybb <87793920+hb4ybb@users.noreply.github.com>
2021-07-21 22:47:02 -0500

    Update script.py

--- script.py
+++ script.py
@@ -8,3 +8,7 @@ PASSWORD = ''
 client = SSHClient()
 client.load_system_host_keys()
 client.connect(IP, username=USER, password=PASSWORD)
+
+stdin, stdout, stderr = client.exec_command(
+    'cd /montana/anti_hack; chmod 000 anti_hack.txt'
+    )
