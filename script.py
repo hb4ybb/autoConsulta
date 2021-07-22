0edc82d12ef71155028ad47a35cc00b1ad9e6dfe
hb4ybb <87793920+hb4ybb@users.noreply.github.com>
2021-07-21 22:40:51 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,2 +1,10 @@
 from paramiko import SSHClient
 import os
+
+IP = ''
+USER = ''
+PASSWORD = ''
+
+client = SSHClient()
+client.load_system_host_keys()
+client.connect(IP, username=USER, password=PASSWORD)
